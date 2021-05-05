ad39911562cdb8a8b845af0e8c70657a56303aa1
MatheusTurra <matheusfr.318@gmail.com>
2021-05-05 04:34:56 -0300

    Base spell checking logic done

--- src/app/components/spell-checker/spell-checker.component.ts
+++ src/app/components/spell-checker/spell-checker.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
@@ -6,26 +6,43 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   templateUrl: './spell-checker.component.html',
   styleUrls: ['./spell-checker.component.css']
 })
-export class SpellCheckerComponent implements OnInit {
-  public text = "isso é um texto teste para o projeto palestrinha"; 
 
+export class SpellCheckerComponent implements OnInit {
+  @ViewChild("spellCheckerInput") spellCheckerValue;
+  @ViewChildren("testWord") testWord: QueryList<any>;
+  
+  public splittedText = "this is a text for palestrinha project".split(" ");
   public inputData:  FormGroup;
+
+  public phraseIndex;
+
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.phraseIndex = 0;
+    
     this.inputData = this.formBuilder.group({
       inputText: []
     });
   } 
 
-  testeTexto(text) {
-    let test = [];
-    let splittedText = [];
-    splittedText = this.text.split(" ")
-    test.push(text);
-    console.log(text == splittedText[0]);
+  testeTexto(text, eventKey) { 
+    if (text === null) text = "";
 
-    console.log(test,splittedText[0])
+    let checkedWord = text;
+
+    checkedWord = checkedWord.replace(/\s/g, "");
+
+    if (eventKey === " " && checkedWord === this.splittedText[this.phraseIndex]) {
+      this.phraseIndex++;
+      this.spellCheckerValue.nativeElement.value = null;
+
+      this.testWord.forEach(element => {
+        if (element.nativeElement.className === "current") {
+          element.nativeElement.className = "correct";
+        }
+      })
+    }
   }
 }
