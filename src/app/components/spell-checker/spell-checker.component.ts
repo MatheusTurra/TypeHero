7779daf6fd1dd9217d186f3f7879e9531f86acf0
MatheusTurra <matheusfr.318@gmail.com>
2021-05-06 18:29:42 -0300

    Basic timer logic done

--- src/app/components/spell-checker/spell-checker.component.ts
+++ src/app/components/spell-checker/spell-checker.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { element } from 'protractor';
 
 @Component({
   selector: 'app-spell-checker',
@@ -15,13 +14,20 @@ export class SpellCheckerComponent implements OnInit {
   public splittedText = "this is a text for palestrinha project".split(" ");
   public inputData:  FormGroup;
 
-  public phraseIndex;
+  public phraseIndex: number;
+
+  public minutes: number;
+  public seconds: number;
+  public chronometer: number;
 
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.phraseIndex = 0;
+    
+    this.minutes = 1;
+    this.seconds = 0;
     
     this.inputData = this.formBuilder.group({
       inputText: []
@@ -46,11 +52,7 @@ export class SpellCheckerComponent implements OnInit {
       this.phraseIndex++;
       this.spellCheckerValue.nativeElement.value = null;
 
-      this.testWord.forEach(element => {
-        if (element.nativeElement.className === "current") {
-          element.nativeElement.className = "incorrectWord";
-        }
-      });
+      this.changeWordToIncorrectClass();
     }
   }
 
@@ -69,5 +71,31 @@ export class SpellCheckerComponent implements OnInit {
         element.nativeElement.className = "incorrectWord";
       }
     })
+  }
+
+ 
+  timerStart() {
+    
+    let secondsInterval = setInterval(() => {
+
+      if (this.minutes === 0 && this.seconds === 0) {
+        console.log("ACABOOU");
+        clearInterval(secondsInterval);
+      }
+
+      
+      if (this.seconds === 0) {
+        this.seconds = 60;
+        this.minutes--;
+
+        if (this.minutes < 0) {
+          this.seconds = 0;
+          this.minutes = 0;
+        }
+      } else {
+        this.seconds--;
+      }
+
+    }, 1000);
   }
 }
