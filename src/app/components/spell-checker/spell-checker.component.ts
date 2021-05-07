e35b81aaf265a1c1368cca271dfb07cff3773027
MatheusTurra <matheusfr.318@gmail.com>
2021-05-07 04:05:15 -0300

    words per minute for correct words logic started

--- src/app/components/spell-checker/spell-checker.component.ts
+++ src/app/components/spell-checker/spell-checker.component.ts
@@ -10,9 +10,16 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class SpellCheckerComponent implements OnInit {
   @ViewChild("spellCheckerInput") spellCheckerValue;
   @ViewChildren("testWord") testWord: QueryList<any>;
-  
-  public splittedText = "this is a text for palestrinha project".split(" ");
+
+  public splittedText = "Beowulf, a young warrior in Geatland (southwestern Sweden), comes to the Scyldings' aid, bringing with him 14 of his finest men. Hrothgar once sheltered Beowulf's father during a deadly feud, and the mighty Geat hopes to return the favor while enhancing his own reputation and gaining treasure for his king, Hygelac. At a feast before nightfall of the first day of the visit, an obnoxious, drunken Scylding named Unferth insults Beowulf and claims that the Geat visitor once embarrassingly lost a swimming contest to a boyhood acquaintance na".split(" ")
+  // public splittedText = "this is a text for palestinha project".split(" ");
   public inputData:  FormGroup;
+  
+  public wpmResult: number
+  
+  public correctWords: number;
+  public incorrectWords: number;
+  public keyStrokes: number;
 
   public phraseIndex: number;
 
@@ -20,15 +27,24 @@ export class SpellCheckerComponent implements OnInit {
   public seconds: number;
   public chronometer: number;
 
+  private startWasClicked: Boolean;
+  private timerInterval;
+
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.phraseIndex = 0;
+
+    this.keyStrokes = 0;
+    this.correctWords = 0;
+    this.incorrectWords = 0;
     
     this.minutes = 1;
     this.seconds = 0;
-    
+
+    this.startWasClicked = true;
+
     this.inputData = this.formBuilder.group({
       inputText: []
     });
@@ -37,22 +53,47 @@ export class SpellCheckerComponent implements OnInit {
   testeTexto(text, eventKey) { 
     if (text === null) text = "";
 
+    this.timerStart();
+
     let checkedWord = text;
-    // this.changeWordToCorrectClass();
     checkedWord = checkedWord.replace(/\s/g, "");
 
+
     if (eventKey === " " && checkedWord === this.splittedText[this.phraseIndex]) {
+
+      this.keyStrokes += this.splittedText[this.phraseIndex].length
+     
       this.phraseIndex++;
+      this.correctWords++;
+     
       this.spellCheckerValue.nativeElement.value = null;
       
       this.changeWordToCorrectClass();
     } 
     else if (eventKey === " " && checkedWord !== this.splittedText[this.phraseIndex]){
-      // TRANSFORMAR EM FUNÇÃO 
       this.phraseIndex++;
-      this.spellCheckerValue.nativeElement.value = null;
+       this.spellCheckerValue.nativeElement.value = null;
+      this.incorrectWords++;
 
       this.changeWordToIncorrectClass();
+    }
+  }
+
+  wordsPerMinuteCalc(minutes, seconds) {
+    let countdown = 60 - seconds
+
+    if (countdown !== 60) {
+
+      
+      if (seconds >= 45) {
+        seconds = Math.round(seconds / 60 * 1e2) / 1e2;
+
+      } else {
+        seconds = 1;
+      }
+
+      this.wpmResult = Math.floor((this.keyStrokes /  5) / seconds);
+
     }
   }
 
@@ -75,27 +116,54 @@ export class SpellCheckerComponent implements OnInit {
 
  
   timerStart() {
-    
-    let secondsInterval = setInterval(() => {
+    if(this.startWasClicked) {
+      this.timerReset();
+     
+      this.startWasClicked = false;
+     
+      this.timerInterval = setInterval(() => {
 
-      if (this.minutes === 0 && this.seconds === 0) {
-        console.log("ACABOOU");
-        clearInterval(secondsInterval);
-      }
+        this.wordsPerMinuteCalc(this.minutes, this.seconds);
 
-      
-      if (this.seconds === 0) {
-        this.seconds = 60;
-        this.minutes--;
+        if (this.minutes === 0 && this.seconds === 0) {
+          this.startWasClicked = true;
 
-        if (this.minutes < 0) {
-          this.seconds = 0;
-          this.minutes = 0;
+          this.spellCheckerValue.nativeElement.className = "timeEnded";
+          this.spellCheckerValue.nativeElement.value = null;
+          this.inputData.disable();
+
+          this.timerStop();
         }
-      } else {
-        this.seconds--;
-      }
-
-    }, 1000);
+  
+        
+        if (this.seconds === 0) {
+          this.seconds = 60;
+          this.minutes--;
+  
+          if (this.minutes < 0) {
+            this.seconds = 0;
+            this.minutes = 0;
+          }
+        } else {
+          this.seconds--;
+        }
+      }, 1000);
+    }
   }
+
+  timerReset() {
+    this.startWasClicked = true;
+    this.minutes = 1;
+    this.seconds = 0;
+
+    this.spellCheckerValue.nativeElement.className = null;
+    this.inputData.enable();
+
+    this.timerStop();
+  }
+
+  timerStop() {
+    clearInterval(this.timerInterval);
+  }
+
 }
